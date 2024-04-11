6b2fd26372c74c9bb1d02d1a906e78d18080d885
rt65s <rt65s@SCHOOL_LAPTOP>
2024-04-10 19:45:12 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prog122_Assign1_Milender
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
