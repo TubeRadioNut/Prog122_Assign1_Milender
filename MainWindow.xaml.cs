fadde12772d1d35350f0bb0a9024f4b06e3294c3
rt65s <rt65s@SCHOOL_LAPTOP>
2024-04-11 17:39:49 -0700

    Updated Eveything

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,38 @@ namespace Prog122_Assign1_Milender
             InitializeComponent();//<--Don't delete this
         }//end of the MainWindow
 
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //get user input, parse it, perform addtion and display in messagebox
+            try
+            {
+                double num1 = double.Parse(txtNumber1.Text);
+                double num2 = double.Parse(txtNumber2.Text);
+                double sum = num1 + num2;
+                MessageBox.Show($"{num1.ToString()} + {num2.ToString()} = {sum.ToString()}");
+            }
+            catch(FormatException fex)
+            {
+                MessageBox.Show(fex.Message + " Please enter a number or a number and decimal only!");
+            }
+            
+        }
 
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //get user input, parse it, perform subtraction and display in messagebox
+            try
+            {
+                double num1 = double.Parse(txtNumber1.Text);
+                double num2 = double.Parse(txtNumber2.Text);
+                double sum = num1 - num2;
+                MessageBox.Show($"{num1.ToString()} - {num2.ToString()} = {sum.ToString()}");
+            }
+            catch (FormatException fex)
+            {
+                MessageBox.Show(fex.Message + " Please enter a number or a number and decimal only!");
+            }
+        }
     }//end of the class
 }//end of namespace
