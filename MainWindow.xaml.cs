6dc1894faf9b1eb5571aa201ac95ee0b6c437580
rt65s <rt65s@SCHOOL_LAPTOP>
2024-04-12 10:10:49 -0700

    Updated Comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,26 +25,36 @@ namespace Prog122_Assign1_Milender
             InitializeComponent();//<--Don't delete this
         }//end of the MainWindow
 
-        //Create a method for the add button
+        /// <summary>
+        /// Create method for the add button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             //get user input, parse it in try, catch exception to display it and inform user how to correct input,
             //perform addition and display in messagebox
             try
             {
+                //Creates variables for user input and parse to double
                 double num1 = double.Parse(txtNumber1.Text);
                 double num2 = double.Parse(txtNumber2.Text);
                 double sum = num1 + num2;
                 MessageBox.Show($"{num1.ToString()} + {num2.ToString()} = {sum.ToString()}");
             }
-            catch(FormatException fex)
+            //Creates catch method to inform user to input number, number and decimal, or just decimal
+            catch (FormatException fex)
             {
                 MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
             }
             
         }
 
-        //Create a method for the subtract button
+        /// <summary>
+        /// Create a method for the subtract button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSubtract_Click(object sender, RoutedEventArgs e)
         {
 
@@ -52,44 +62,58 @@ namespace Prog122_Assign1_Milender
             //perform subtraction and display in messagebox
             try
             {
+                //Creates variables for user input and parse to double
                 double num1 = double.Parse(txtNumber1.Text);
                 double num2 = double.Parse(txtNumber2.Text);
                 double sum = num1 - num2;
                 MessageBox.Show($"{num1.ToString()} - {num2.ToString()} = {sum.ToString()}");
             }
+            //Creates catch method to inform user to input number, number and decimal, or just decimal
             catch (FormatException fex)
             {
                 MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
             }
         }
 
-        //Create a method for the multiply button
+        /// <summary>
+        /// Create a method for the multiply button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnMultiply_Click(object sender, RoutedEventArgs e)
         {
             //get user input, parse it in try, catch exception to display it and inform user how to correct input,
             //perform multiplication and display in messagebox
             try
             {
+                //Creates variables for user input and parse to double
                 double num1 = double.Parse(txtNumber1.Text);
                 double num2 = double.Parse(txtNumber2.Text);
                 double sum = num1 * num2;
                 MessageBox.Show($"{num1.ToString()} * {num2.ToString()} = {sum.ToString()}");
             }
+            //Creates catch method to inform user to input number, number and decimal, or just decimal
             catch (FormatException fex)
             {
                 MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
             }
         }
-
-        //Create a method for the divide button
+        
+        /// <summary>
+        /// Create a method for the divide button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnDivide_Click(object sender, RoutedEventArgs e)
         {
             //get user input, parse it in try, catch exception to display it and inform user how to correct input,
             //perform division and display in messagebox, use if else for division by zero to inform user not to do that
             try
             {
+                //Creates variables for user input and parse to double
                 double num1 = double.Parse(txtNumber1.Text);
                 double num2 = double.Parse(txtNumber2.Text);
+                //creates the if else method to deal with divide by zero
                 if (num2 != 0)
                 {
                     double sum = num1 / num2;
@@ -101,6 +125,7 @@ namespace Prog122_Assign1_Milender
                 }
                 
             }
+            //Creates catch method to inform user to input number, number and decimal, or just decimal
             catch (FormatException fex)
             {
                 MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
