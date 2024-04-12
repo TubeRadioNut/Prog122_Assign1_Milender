58d3cfd9fff733a936f968da19f351c9b474cf0f
rt65s <rt65s@SCHOOL_LAPTOP>
2024-04-11 22:28:18 -0700

    Updated Everything Again

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,9 +25,11 @@ namespace Prog122_Assign1_Milender
             InitializeComponent();//<--Don't delete this
         }//end of the MainWindow
 
+        //Create a method for the add button
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            //get user input, parse it, perform addtion and display in messagebox
+            //get user input, parse it in try, catch exception to display it and inform user how to correct input,
+            //perform addition and display in messagebox
             try
             {
                 double num1 = double.Parse(txtNumber1.Text);
@@ -37,15 +39,17 @@ namespace Prog122_Assign1_Milender
             }
             catch(FormatException fex)
             {
-                MessageBox.Show(fex.Message + " Please enter a number or a number and decimal only!");
+                MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
             }
             
         }
 
+        //Create a method for the subtract button
         private void btnSubtract_Click(object sender, RoutedEventArgs e)
         {
-            
-            //get user input, parse it, perform subtraction and display in messagebox
+
+            //get user input, parse it in try, catch exception to display it and inform user how to correct input,
+            //perform subtraction and display in messagebox
             try
             {
                 double num1 = double.Parse(txtNumber1.Text);
@@ -55,7 +59,51 @@ namespace Prog122_Assign1_Milender
             }
             catch (FormatException fex)
             {
-                MessageBox.Show(fex.Message + " Please enter a number or a number and decimal only!");
+                MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
+            }
+        }
+
+        //Create a method for the multiply button
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            //get user input, parse it in try, catch exception to display it and inform user how to correct input,
+            //perform multiplication and display in messagebox
+            try
+            {
+                double num1 = double.Parse(txtNumber1.Text);
+                double num2 = double.Parse(txtNumber2.Text);
+                double sum = num1 * num2;
+                MessageBox.Show($"{num1.ToString()} * {num2.ToString()} = {sum.ToString()}");
+            }
+            catch (FormatException fex)
+            {
+                MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
+            }
+        }
+
+        //Create a method for the divide button
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            //get user input, parse it in try, catch exception to display it and inform user how to correct input,
+            //perform division and display in messagebox, use if else for division by zero to inform user not to do that
+            try
+            {
+                double num1 = double.Parse(txtNumber1.Text);
+                double num2 = double.Parse(txtNumber2.Text);
+                if (num2 != 0)
+                {
+                    double sum = num1 / num2;
+                    MessageBox.Show($"{num1.ToString()} / {num2.ToString()} = {sum.ToString()}");
+                }
+                else
+                {
+                    MessageBox.Show("Can not divide by zero, try again!");
+                }
+                
+            }
+            catch (FormatException fex)
+            {
+                MessageBox.Show(fex.Message + " Please enter a number or a number and/or decimal only!");
             }
         }
     }//end of the class
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Prog122_Assign1_Milender
+Used double variables
+used try catch for parse/exception handling
+since divide by zero with double does not throw an exception, used if else to handle divide by zero
